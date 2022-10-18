3ff6047b3131d4209e2aeca2fb0dfe4e5676299d
Dominic Hopton <d.hopton@codevoid.net>
2022-10-18 13:34:18 -0700

    Implement basic source output w/ indentation tracking
    Plus some unit tests

--- Reswinator/OutputGenerator.cs
+++ Reswinator/OutputGenerator.cs
@@ -1,5 +1,50 @@
-﻿namespace Codevoid.Utilities.Reswinator;
+﻿using System.Text;
+
+namespace Codevoid.Utilities.Reswinator;
 
 public class OutputGenerator
 {
+    private StringBuilder builder = new StringBuilder();
+    private uint indentLevel = 0;
+
+    public string GetOutput() => builder.ToString();
+
+    public void Indent()
+    {
+        indentLevel += 1;
+    }
+
+    public void Dindent()
+    {
+        if(indentLevel < 1)
+        {
+            throw new NotSupportedException("You can't have an indent level less than zero");
+        }
+
+        indentLevel -= 1;
+    }
+
+    private void WriteIndent()
+    {
+        if(indentLevel < 1)
+        {
+            return;
+        }
+
+        for (var level = 0; level < indentLevel; level += 1)
+        {
+            builder.Append("    ");
+        }
+    }
+
+    public void NewLine()
+    {
+        builder.AppendLine(String.Empty);
+    }
+
+    public void WriteLine(string line)
+    {
+        this.WriteIndent();
+        builder.AppendLine(line);
+    }
 }
--- ReswinatorTests/OutputGenerator.Tests.cs
+++ ReswinatorTests/OutputGenerator.Tests.cs
@@ -4,10 +4,75 @@ namespace Codevoid.Tests.Reswinator;
 
 public class OutputGeneratorTests
 {
+    private OutputGenerator generator = new OutputGenerator();
+
     [Fact]
-    public void CanInstantiate()
+    public void GettingOutputWithoutWritingAnythingGivesEmptyString()
     {
-        var outputGenerator = new OutputGenerator();
-        Assert.NotNull(outputGenerator);
+        Assert.Empty(this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void IndentingButNotWritingReturnsEmptyString()
+    {
+        this.generator.Indent();
+        Assert.Empty(this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void DindentingWithNoIndentThrowsException()
+    {
+        Assert.Throws<NotSupportedException>(() => this.generator.Dindent());
+    }
+
+    [Fact]
+    public void WritingLineAddsToOutput()
+    {
+        this.generator.WriteLine("Hello");
+        Assert.Equal("Hello\n", this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void WritingLineWithIndentIncludesIndentBeforeWrittenLine()
+    {
+        this.generator.Indent();
+        this.generator.WriteLine("Hello");
+        Assert.Equal("    Hello\n", this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void WritingLineWithTwoIndentLevelsIncludesIndentBeforeWrittenLine()
+    {
+        this.generator.Indent();
+        this.generator.Indent();
+        this.generator.WriteLine("Hello");
+        Assert.Equal("        Hello\n", this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void IndentLevevelChangesInterleavedWithWritesAreRespected()
+    {
+        this.generator.Indent();
+        this.generator.Indent();
+        this.generator.WriteLine("Hello");
+        this.generator.Dindent();
+        this.generator.Dindent();
+        this.generator.WriteLine("World");
+        Assert.Equal("        Hello\nWorld\n", this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void WritingNewLineOnlyAddsOnlyNewLineToOutput()
+    {
+        this.generator.NewLine();
+        Assert.Equal("\n", this.generator.GetOutput());
+    }
+
+    [Fact]
+    public void WritingNewLineWithIndentOnlyAddsOnlyNewLineToOutput()
+    {
+        this.generator.Indent();
+        this.generator.NewLine();
+        Assert.Equal("\n", this.generator.GetOutput());
     }
 }
